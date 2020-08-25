739bd43c7cf541d806dc19f2d61d679dfd7a6951
Sara A <calisaraalhindi@gmail.com>
2020-08-25 20:34:44 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,6 +10,6 @@ class EmailAddressParser
   end
   
   def parse
-    @cvs_emails.split(",").collect {|s| s.split(',')}.flatten.uniq
+    @cvs_emails.split(",").collect {|s| s.split(',')}
   end
 end
